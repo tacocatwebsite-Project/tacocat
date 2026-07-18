3a96ed4409e658414d82d63da2b2383f58ea3241
tacocatwebsite-Project <tacocatwebsite@gmail.com>
2026-07-17 21:56:09 -0400

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -3,7 +3,7 @@ const SUPABASE_URL = "https://ztrgdltugiiityatvxpe.supabase.co";
 const SUPABASE_KEY = "sb_publishable_XPNsaBSY44-obehxipQ9mw_xSn0wVBN";
 
 
-const supabaseClient = supabase.createClient(
+window.supabaseClient = supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
